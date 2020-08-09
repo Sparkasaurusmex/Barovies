32c65655bc770d81c2d89d1384d1dd8e1c620e3c
Sparkasaurusmex <66787760+Sparkasaurusmex@users.noreply.github.com>
2020-08-09 01:12:46 -0500

    Add files via upload

---
+++ script/Barovies.js
@@ -0,0 +1,25 @@
+Hooks.on('renderTidy5eSheet', (sheet, html) => {
+  html.find('.denomination.pp').remove();
+  html.find('[name="data.currency.pp"]').remove();
+  html.find('.denomination.ep').text('Barovies');
+  // etc.
+});
+
+Hooks.once('ready', () => {
+  CONFIG.Actor.sheetClasses.character['dnd5e.Tidy5eSheet'].cls.prototype._onConvertCurrency = _onMyConvertCurrency;
+});
+
+  function _onMyConvertCurrency(event) {
+    event.preventDefault();
+    const curr = duplicate(this.actor.data.data.currency);
+    console.log(curr);
+    const convert = {
+      ep: {into: "gp", each: 2}
+    };
+    for ( let [c, t] of Object.entries(convert) ) {
+      let change = Math.floor(curr[c] / t.each);
+      curr[c] -= (change * t.each);
+      curr[t.into] += change;
+    }
+    return this.actor.update({"data.currency": curr});
+ };
